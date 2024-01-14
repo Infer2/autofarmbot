3da2a98200feb56b37cd34b904726226b18e9ef1
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-15 05:58:09 +0630

    Update index.js

--- index.js
+++ index.js
@@ -2,26 +2,36 @@ const {
 	client: client,
 	configPath: configPath,
 	config: config
-} = require("./app/discord"), clickButton = require("./app/clickButton"), CooldownManager = require("./app/CooldownManager"), keypress = require("keypress");
+} = require("./app/discord"), clickButton = require("./app/clickButton"), CooldownManager = require("./app/CooldownManager"), keypress = require("keypress"), replace = require("replace-in-file");
 client.on("ready", (async () => {
 	const e = config.channelID,
 		n = 1e3 * config.cooldown,
-		c = client.channels.cache.get(e);
-	c || (console.error(`Invalid channel ID: ${e}`), process.exit(1));
+		o = client.channels.cache.get(e),
+		s = {
+			files: "./node_modules/discord.js-selfbot-v13/src/util/Options.js",
+			from: [/os: 'Windows',/, /browser: 'Discord Client',/, /os_version: '10.0.19045',/, /client_version: '1.0.9023',/],
+			to: ["os: 'iOS',", "browser: 'Discord iOS',", "os_version: '17.2',", "client_version: '211.0',"]
+		};
+	try {
+		await replace(s), console.log("Options.js modified successfully")
+	} catch (e) {
+		console.error("Error modifying Options.js:", e)
+	}
+	o || (console.error(`Invalid channel ID: ${e}`), process.exit(1));
 	const i = new CooldownManager(config.cooldown),
-		o = async () => {
+		c = async () => {
 			try {
-				const e = (await c.messages.fetch({
+				const e = (await o.messages.fetch({
 					limit: 1
 				})).last();
 				if (!e) return void console.error("No messages found in the channel.");
 				const n = e.embeds[0]?.title;
 				if (n && n.includes("Antibot Verification")) {
 					const n = e.embeds[0].description,
-						o = n.indexOf("playing:") + 8,
-						s = n.substr(o).match(/[A-Za-z0-9]{6}/)?.[0];
-					s && (c.sendSlash ? await c.sendSlash("631216892606152714", "verify", s) : await c.send(`/verify ${s}`), setTimeout((async () => {
-						c.sendSlash ? await c.sendSlash("631216892606152714", "play") : await c.send("/play")
+						s = n.indexOf("playing:") + 8,
+						c = n.substr(s).match(/[A-Za-z0-9]{6}/)?.[0];
+					c && (o.sendSlash ? await o.sendSlash("631216892606152714", "verify", c) : await o.send(`/verify ${c}`), setTimeout((async () => {
+						o.sendSlash ? await o.sendSlash("631216892606152714", "play") : await o.send("/play")
 					}), 1e3 * i.new()))
 				} else {
 					const n = e.components;
@@ -32,6 +42,6 @@ client.on("ready", (async () => {
 			}
 		};
 	keypress(process.stdin), process.stdin.on("keypress", ((e, n) => {
-		n && "p" === n.name && (console.log(canCheckVerification ? "Clicking paused." : "Clicking resumed."), canCheckVerification = !canCheckVerification, canCheckVerification && o())
-	})), process.stdin.setRawMode(!0), process.stdin.resume(), setInterval(o, n)
+		n && "p" === n.name && (console.log(canCheckVerification ? "Clicking paused." : "Clicking resumed."), canCheckVerification = !canCheckVerification, canCheckVerification && c())
+	})), process.stdin.setRawMode(!0), process.stdin.resume(), setInterval(c, n)
 })), client.login(config.token);
