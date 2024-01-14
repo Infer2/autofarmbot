a13efc39c70a664ffac8662f77861e503a1383d7
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-15 05:24:08 +0630

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,8 +1,6 @@
-
-const MARGIN = Math.random() * (0.35 - 0.1) + 0.1;
-const SIGMA = 0.12289;
-
+const MARGIN = Math.random() * (.35 - .1) + .1,
+	SIGMA = .12289;
 module.exports = {
-  MARGIN,
-  SIGMA,
+	MARGIN: MARGIN,
+	SIGMA: SIGMA
 };
