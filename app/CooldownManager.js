cc045958c446b1515c7018325b1a8cb153f9a568
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-15 05:23:29 +0630

    Update CooldownManager.js

--- app/CooldownManager.js
+++ app/CooldownManager.js
@@ -1,49 +1,33 @@
-
-const { MARGIN, SIGMA } = require('./config');
-
+const {
+	MARGIN: MARGIN,
+	SIGMA: SIGMA
+} = require("./config");
 class CooldownManager {
-  constructor(userCooldown) {
-    this.userCooldown = userCooldown;
-    this.mu = MARGIN + this.userCooldown;
-    this.sigma = SIGMA;
-    this.margin = MARGIN;
-    this.seed = Math.floor(Math.random() * (10 ** 7 - 10 ** 5 + 1) + 10 ** 5);
-    this.dataset = [];
-  }
-
-  get last() {
-    return this.dataset.length > 0 ? this.dataset[this.dataset.length - 1] : null;
-  }
-
-  analysis() {
-    if (this.dataset.length > 2) {
-      const sampleStdev = standardDeviation(this.dataset);
-      const sampleMean = mean(this.dataset);
-      return [
-        [sampleStdev, this.sigma - sampleStdev],
-        [sampleMean, this.mu - sampleMean],
-        variance(this.dataset)
-      ];
-    }
-  }
-
-  new() {
-    const value = this.gauss(this.mu, this.sigma);
-    this.dataset.push(value);
-    return value;
-  }
-
-  gauss(mu, sigma) {
-    const rand = Math.sqrt(-2.0 * Math.log(Math.random())) * Math.cos(2.0 * Math.PI * Math.random());
-    return mu + sigma * rand;
-  }
-
-  custom(mu, sigma = null) {
-    if (!sigma) {
-      sigma = Math.random() * (3 - 1) + 1;
-    }
-    return this.gauss(mu, sigma);
-  }
+	constructor(t) {
+		this.userCooldown = t, this.mu = MARGIN + this.userCooldown, this.sigma = SIGMA, this.margin = MARGIN, this.seed = Math.floor(9900001 * Math.random() + 10 ** 5), this.dataset = []
+	}
+	get last() {
+		return this.dataset.length > 0 ? this.dataset[this.dataset.length - 1] : null
+	}
+	analysis() {
+		if (this.dataset.length > 2) {
+			const t = standardDeviation(this.dataset),
+				s = mean(this.dataset);
+			return [
+				[t, this.sigma - t],
+				[s, this.mu - s], variance(this.dataset)
+			]
+		}
+	}
+	new() {
+		const t = this.gauss(this.mu, this.sigma);
+		return this.dataset.push(t), t
+	}
+	gauss(t, s) {
+		return t + s * (Math.sqrt(-2 * Math.log(Math.random())) * Math.cos(2 * Math.PI * Math.random()))
+	}
+	custom(t, s = null) {
+		return s || (s = 2 * Math.random() + 1), this.gauss(t, s)
+	}
 }
-
 module.exports = CooldownManager;
